526b2cbbf2061d6fc235df3012e86138cc418dbb
Jay <jayjethava.tst@gmail.com>
2024-12-06 18:02:43 +0530

    add rate limiter and change the end point of the get all users

--- src/modules/users/index.js
+++ src/modules/users/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const userController = require("./controller");
 
 router.post("/add-new-user", userController.addUser);
-router.get("/user-list", userController.getAllUsers);
+router.get("/", userController.getAllUsers);
 router.route("/:id").patch(userController.updateById);
 
 module.exports = router;

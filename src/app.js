e2b1325484ebe5e4df9e2c5d1b851e55a61667ce
Jay <jayjethava.tst@gmail.com>
2024-12-10 17:23:24 +0530

    change the name in package.json

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const limiter = rateLimit({
 
 // Global middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // parse from data
 app.use(cors());
 app.use(limiter);
 
